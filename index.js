0cda092a7804af425a9048349ccc04484880f030
SatyajeetGhosh <satyajeetghosh5331@gmail.com>
2023-04-03 22:24:58 +0530

    Added comments for complex parts of code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
-let lockBoard = false;
+let lockBoard = false; //This variable is declared to take care of corner cases
 let firstCard, secondCard;
 
 function flipCard(){
     if(lockBoard) return;
+
+    //Corner Case 2: If we double click on the same card the match is gonna evaluate to true and its gonna disble the card by removing its event listener
     if(this === firstCard) return;
 
     this.classList.add('flip');
@@ -23,9 +25,15 @@ function flipCard(){
     checkForMatch();
 }
 
+//In order to access that we defined in HTMl we use the dataset object 
+//Syntax = element.dataset.name
+//
 function checkForMatch(){
     // Checking if both cards match
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+
+    //We're using ternary operator here
+    //syntax = condition:statemnts to be executed if condition is true:statements to be executed if condition is false
     isMatch ? disableCards() : unflipCards();
 }
 
@@ -38,9 +46,12 @@ function disableCards(){
 }
 
 function unflipCards(){
-    lockBoard = true;
+    //We're locking the board and unlocking it after the 2 cards have unflipped
+    //Corner Case 1: This is done to prevent the user to flip a 2nd set of cards before the 1st set has unflipped or otherwise it crashes our logic
+    lockBoard = true; 
 
-    //not a match
+    //If not a match we flip the cards back
+    //setTimeout() method calls a function after a number of milliseconds
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
@@ -50,10 +61,16 @@ function unflipCards(){
 }
 
 function resetBoard(){
+    //The following lines of code is the syntax for ES6 destructuring assignment
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
+//Shuffling the cards using a flexbox property called order
+//Math.random() function is used to generate a random number from 0 to 1 excluding 1
+//We want a number from 1 to 11 so we multiply the result of above by 12
+//Math.floor() is used because we need an integer
+//(function functionName(){...})(); = This syntax makes the shuffle function an immediately invoked function expression i.e this function will be executed write after its definition
 (function shuffle() {
     cards.forEach(card => {
       let randomPos = Math.floor(Math.random() * 12);
